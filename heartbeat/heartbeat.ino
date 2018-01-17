f962c94d035f13b85f51ac3f22f0558e2a519f59
saraholsen <saraholsen@sarahs-mbp.dhcp.wustl.edu>
2018-01-17 14:02:36 -0600

    Heartbeat with Blinking light

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -1,17 +1,22 @@
 void setup() {
   // put your setup code here, to run once:
   //int n = 0;
-  Serial.begin(9600);  
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);   
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1001);
   int n = millis(); 
   n = n / 1000; 
+  digitalWrite(13, HIGH); 
+  delay(400);
+  digitalWrite(13, LOW);
+  delay(400);  
   Serial.print(n); 
   Serial.println(" seconds have elapsed"); 
+  delay(200); 
   
 
 }
